4593a088fc6ebc82377009a2f4b8b8dd3e9aa2de
Bernd Ahlers <bernd@tuneafish.de>
2011-09-25 12:08:44 +0200

    Add mutt recipe.

---
+++ mutt/recipe.rb
@@ -0,0 +1,29 @@
+class Mutt < FPM::Cookery::Recipe
+  description 'small but very powerful text-based mail client'
+
+  name     'mutt'
+  version  '1.5.20'
+  homepage 'http://www.mutt.org'
+  source   'ftp://ftp.mutt.org/mutt/devel/mutt-1.5.20.tar.gz'
+
+  conflicts 'mutt'
+  replaces  'mutt'
+
+  exclude   'etc/mime.types*'
+
+  def build
+    configure :prefix => prefix,
+              'enable-hcache' => true,
+              'enable-imap' => true,
+              'with-ssl' => true,
+              :sysconfdir => etc
+    make
+  end
+
+  def install
+    inline_replace 'Makefile' do |s|
+      s.gsub! '$(MAKE) $(AM_MAKEFLAGS) install-exec-hook', '# disabled install-exec-hook'
+    end
+    make :install, 'DESTDIR' => destdir
+  end
+end
