201bc1b4e4807aff48caccfe67387e941a1865f8
Bernd Ahlers <bernd@tuneafish.de>
2011-10-19 00:01:45 +0200

    Add sha1 for tmux.

--- tmux/recipe.rb
+++ tmux/recipe.rb
@@ -5,6 +5,7 @@ class Tmux < FPM::Cookery::Recipe
   version  '1.5'
   homepage 'http://tmux.sourceforge.net/'
   source   'http://downloads.sourceforge.net/project/tmux/tmux/tmux-1.5/tmux-1.5.tar.gz'
+  sha1     'e26132f02f344f8d6d06ae87d11f6a7aad880f67'
 
   conflicts 'tmux'
   replaces  'tmux'
