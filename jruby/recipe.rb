6a624174532121150e371fe92fe14b2f6d11f3ce
Bernd Ahlers <bernd@tuneafish.de>
2011-10-19 00:02:37 +0200

    Create a jruby wrapper script.

--- jruby/recipe.rb
+++ jruby/recipe.rb
@@ -3,6 +3,7 @@ class JRuby < FPM::Cookery::Recipe
 
   name     'jruby'
   version  '1.6.4'
+  revision 2
   homepage 'http://www.jruby.org/'
   source   "http://jruby.org.s3.amazonaws.com/downloads/#{version}/jruby-bin-#{version}.tar.gz"
   arch     'all'
@@ -18,5 +19,16 @@ class JRuby < FPM::Cookery::Recipe
 
   def install
     prefix("jruby-#{version}").install Dir['*']
+
+    bin.mkdir
+    File.open(bin('jruby'), 'w') do |f|
+      with_trueprefix do
+        f.write <<-__SHELL
+#!/bin/bash
+exec #{prefix("jruby-#{version}/bin/jruby")} "$@"
+        __SHELL
+      end
+    end
+    chmod 0555, bin('jruby')
   end
 end
