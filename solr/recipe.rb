38a9858066b0a1de39d5d455979960ce40106b1f
Bernd Ahlers <bernd@tuneafish.de>
2011-10-24 12:02:56 +0200

    Add solr recipe. (from brew2deb)

---
+++ solr/recipe.rb
@@ -0,0 +1,41 @@
+# Adapted from the formula in brew2deb.
+
+class Solr < FPM::Cookery::Recipe
+  description 'Enterprise search platform'
+
+  name     'solr'
+  version  '3.3.0'
+  homepage 'http://lucene.apache.org/solr/'
+  source   "http://www.eng.lsu.edu/mirrors/apache/lucene/solr/#{version}/apache-solr-#{version}.tgz"
+  md5      '048e2aa7e16358600fa5fe3570b90018'
+
+  section 'database'
+
+  build_depends 'sun-java6-jdk'
+  depends       'sun-java6-jre'
+
+  def build
+    warfile = File.expand_path('example/webapps/solr.war')
+
+    FileUtils.mkdir_p 'war'
+    Dir.chdir 'war' do
+      system 'jar', 'xvf', warfile
+    end
+  end
+
+  def install
+    solr_home = share('solr')
+    (solr_home/'lib').install Dir['dist/*.{war,jar}']
+
+    Dir.chdir('example') do
+      (solr_home/'jetty').install Dir['{start.jar,etc,lib,webapps}']
+    end
+
+    lucene_home = share('lucene')
+    share('lucene/lib').install Dir['war/WEB-INF/lib/lucene-*.jar']
+
+    bin.install Dir[workdir('solr*')]
+
+    var('log/solr').mkpath
+  end
+end
