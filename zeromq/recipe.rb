8a0f39de93944e343aae18954926e9fe3730297c
Bernd Ahlers <bernd@tuneafish.de>
2011-10-27 17:47:13 +0200

    Update zeromq to 2.1.10.

--- zeromq/recipe.rb
+++ zeromq/recipe.rb
@@ -2,10 +2,10 @@ class ZeroMQ < FPM::Cookery::Recipe
   description 'The Intelligent Transport Layer'
 
   name     'zeromq'
-  version  '2.1.9'
+  version  '2.1.10'
   homepage 'http://www.zeromq.org'
-  source   'http://download.zeromq.org/zeromq-2.1.9.tar.gz'
-  sha256   'f3542f756687e622beef3a75c8e027fe2d95d4654350cbca4c070ffc58d9ace0'
+  source   'http://download.zeromq.org/zeromq-2.1.10.tar.gz'
+  sha256   'c6727ee334b572b25269c7fed24a926cde236b3655e22f21c057aad4df5a4b79'
 
   conflicts 'libzmq0', 'zeromq-bin'
   replaces  'libzmq0', 'zeromq-bin'
