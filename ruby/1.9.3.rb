304b51611fc538d2b5450c4d94faa49950edf542
Bernd Ahlers <bernd@tuneafish.de>
2011-10-30 16:12:41 +0100

    Update to ruby-1.9.3p0.

--- ruby/1.9.3.rb
+++ ruby/1.9.3.rb
@@ -2,10 +2,10 @@ class Ruby193 < FPM::Cookery::Recipe
   description 'The Ruby virtual machine'
 
   name 'ruby'
-  version '1.9.3rc1'
+  version '1.9.3.0'
   homepage 'http://www.ruby-lang.org/'
-  source 'ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-rc1.tar.bz2'
-  sha256 '951a8810086abca0e200f81767a518ee2730d6dc9b0cc2c7e3587dcfc3bf5fc8'
+  source 'http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p0.tar.bz2'
+  sha256 'ca8ba4e564fc5f98b210a5784e43dfffef9471222849e46f8e848b37e9f38acf'
 
   section 'interpreters'
 
