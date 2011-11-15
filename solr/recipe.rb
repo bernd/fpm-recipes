a67ff1b7c99417632fa9364606dc66d0abe2a322
Bernd Ahlers <bernd@tuneafish.de>
2011-11-15 14:27:53 +0100

    Fix library paths and bump revision.

--- solr/recipe.rb
+++ solr/recipe.rb
@@ -5,6 +5,7 @@ class Solr < FPM::Cookery::Recipe
 
   name     'solr'
   version  '3.4.0'
+  revision 1
   homepage 'http://lucene.apache.org/solr/'
   source   "http://www.eng.lsu.edu/mirrors/apache/lucene/solr/#{version}/apache-solr-#{version}.tgz"
   md5      '949b145669a6c9517b2fef32b58f679e'
