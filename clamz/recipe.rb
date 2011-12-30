62ea64e1f4181832dab2a30fc0c31b13f340e714
Bernd Ahlers <bernd@tuneafish.de>
2011-12-30 10:31:24 +0100

    Add recipe for clamz-0.5.

---
+++ clamz/recipe.rb
@@ -0,0 +1,20 @@
+class Clamz < FPM::Cookery::Recipe
+  description 'Downloader for the Amazon.com MP3 music store'
+
+  name     'clamz'
+  version  '0.5'
+  homepage 'http://code.google.com/p/clamz/'
+  source   'http://clamz.googlecode.com/files/clamz-0.5.tar.gz'
+  sha256   '5a63f23f15dfa6c2af00ff9531ae9bfcca0facfe5b1aa82790964f050a09832b'
+
+  build_depends 'libgcrypt11-dev', 'libcurl4-gnutls-dev', 'libexpat1-dev'
+
+  def build
+    configure :prefix => prefix
+    make
+  end
+
+  def install
+    make :install, 'DESTDIR' => destdir
+  end
+end
