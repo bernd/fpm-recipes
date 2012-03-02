d373603a66decfbd00d6e9f2637f6b03819d8ddc
Bernd Ahlers <bernd@tuneafish.de>
2012-03-02 17:09:29 +0100

    Add beanstalkd 1.5 recipe.

---
+++ beanstalkd/recipe.rb
@@ -0,0 +1,29 @@
+class Beanstalkd < FPM::Cookery::Recipe
+  description 'A simple, fast work queue'
+
+  name     'beanstalkd'
+  version  '1:1.5.0'
+  homepage 'http://kr.github.com/beanstalkd/'
+  source   'http://github.com/kr/beanstalkd', :with => :git, :tag => 'v1.5'
+
+  section      'database'
+  conflicts    'beanstalkd'
+  config_files '/etc/default/beanstalkd'
+
+  post_install   'post-install'
+  post_uninstall 'post-uninstall'
+
+  def build
+    make
+  end
+
+  def install
+    bin.install 'beanstalkd'
+    man1.install 'doc/beanstalkd.1'
+
+    share('doc/beanstalkd').install 'doc/protocol.txt'
+
+    etc('init').install_p workdir('beanstalkd.upstart'), 'beanstalkd.conf'
+    etc('default').install_p workdir('beanstalkd.default'), 'beanstalkd'
+  end
+end
