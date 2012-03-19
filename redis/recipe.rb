f45b9a83b6290514ce91e1f29e52c5ff82942760
Bernd Ahlers <bernd@tuneafish.de>
2012-03-19 19:48:39 +0100

    Update to redis 2.4.9.

--- redis/recipe.rb
+++ redis/recipe.rb
@@ -4,10 +4,10 @@ class Redis < FPM::Cookery::Recipe
   description 'An advanced key-value store'
 
   name     'redis-server'
-  version  '2:2.4.8'
+  version  '2:2.4.9'
   revision 0
   homepage 'http://redis.io/'
-  source   'https://github.com/antirez/redis', :with => :git, :tag => '2.4.8'
+  source   'https://github.com/antirez/redis', :with => :git, :tag => '2.4.9'
 
   section      'database'
   conflicts    'redis-server'
