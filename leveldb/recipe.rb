04e13d6d97b692d9b17650ac3308744e51834476
Bernd Ahlers <bernd@tuneafish.de>
2012-08-15 15:27:06 +0200

    Add checksum and install header files.

--- leveldb/recipe.rb
+++ leveldb/recipe.rb
@@ -3,8 +3,10 @@ class LevelDB < FPM::Cookery::Recipe
 
   name     'leveldb'
   version  '1.5.0'
+  revision '1'
   homepage 'http://leveldb.googlecode.com/'
   source   "https://leveldb.googlecode.com/files/leveldb-#{version}.tar.gz"
+  sha256   'cb73489af84d9d97d3e7d234a2f5e3a0c89ffadb657e7dd9e9047227778e57ba'
 
   build_depends 'libsnappy-dev'
   depends 'libsnappy1'
@@ -15,5 +17,6 @@ class LevelDB < FPM::Cookery::Recipe
 
   def install
     lib.install Dir['libleveldb.*']
+    include('leveldb').install Dir['include/leveldb/*']
   end
 end
