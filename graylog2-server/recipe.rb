98c7ba6eac77d4fd6876495cc8848f9279a9f9d3
Mick Pollard <aussielunix@gmail.com>
2013-02-15 18:55:16 +1100

    updated recipe.rb for graylog2-server 0.10.0

--- graylog2-server/recipe.rb
+++ graylog2-server/recipe.rb
@@ -1,24 +1,24 @@
 class Graylog2Server < FPM::Cookery::Recipe
   homepage    'http://graylog2.org'
-  source      "http://download.graylog2.org/graylog2-server/graylog2-server-#{version}.tar.gz"
-  md5         '499ae16dcae71eeb7c3a30c75ea7a1a6'
-
   name        'graylog2-server'
-  version     '0.9.6p1'
+  version     '0.10.0'
+  source      "http://download.graylog2.org/#{name}/#{name}-#{version}.tar.gz"
+  md5         '7f02dc4ca30dd79289c95c42e248ea12'
+
   revision    '1'
   vendor      'aussielunix'
   maintainer  'Mick Pollard <aussielunix@gmail.com>'
   license     'GPL-3'
   description 'graylog2-server is the server part of an open source log management solution that stores your logs in elasticsearch.'
-  arch	      'all'
+  arch	      'amd64'
   section     'admin'
 
   depends     'java-runtime-headless'
 
-  config_files '/etc/graylog2.conf'
+  config_files '/etc/graylog2.conf elasticsearch.yml.example'
 
   def build
-    inreplace 'bin/graylog2ctl' do |s|
+    inline_replace 'bin/graylog2ctl' do |s|
       s.gsub! 'GRAYLOG2_SERVER_JAR=graylog2-server.jar', 'GRAYLOG2_SERVER_JAR=' + share('graylog2-server/graylog2-server.jar')
       s.gsub! 'LOG_FILE=log/graylog2-server.log', 'LOG_FILE=' + var('log/graylog2-server.log')
     end
@@ -33,6 +33,7 @@ class Graylog2Server < FPM::Cookery::Recipe
     bin.install 'bin/graylog2ctl'
     etc('init').install_p workdir('graylog2-server.upstart'), 'graylog2-server.conf'
     etc.install_p 'graylog2.conf.example', 'graylog2.conf'
+    etc.install_p 'elasticsearch.yml.example', 'graylog2-elasticsearch.yml'
     share('graylog2-server').install workdir('COPYING')
     share('graylog2-server').install workdir('README')
     share('graylog2-server').install 'build_date'
