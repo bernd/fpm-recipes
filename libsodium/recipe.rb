69adff90b2b08d88a74fd8a2dc4376108071ada2
Bernd Ahlers <bernd@tuneafish.de>
2013-10-14 18:11:08 +0200

    Update to libsodium 0.4.3.

--- libsodium/recipe.rb
+++ libsodium/recipe.rb
@@ -2,10 +2,10 @@ class Libsodium < FPM::Cookery::Recipe
   description 'portable, cross-compilable, installable, packageable, API-compatible version of NaCl'
 
   name     'libsodium'
-  version  '0.4.1'
+  version  '0.4.3'
   revision 0
-  source   "http://download.dnscrypt.org/libsodium/releases/libsodium-#{version}.tar.gz"
-  sha256   '65756c7832950401cc0e6ee0e99b165974244e749f40f33d465f56447bae8ce3'
+  source   "https://github.com/jedisct1/libsodium/releases/download/#{version}/libsodium-#{version}.tar.gz"
+  sha256   '599ce19ae6ace2d30aee353b931088f720713c4e1d0b2918ed46de1914fb6042'
   homepage 'https://github.com/jedisct1/libsodium'
   license  'BSD'
 
