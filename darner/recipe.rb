45e7247fc1b5e69b98762072e3bbff731cfacc72
Bernd Ahlers <bernd@tuneafish.de>
2013-11-23 12:49:54 +0100

    Update to darner 0.2.0.

--- darner/recipe.rb
+++ darner/recipe.rb
@@ -2,8 +2,8 @@ class Darner < FPM::Cookery::Recipe
   description 'a very simple message queue server'
 
   name 'darner'
-  version '0.0.df419f3'
-  source 'https://github.com/wavii/darner.git', :with => :git, :ref => 'df419f3'
+  version '0.2.0'
+  source 'https://github.com/wavii/darner.git', :with => :git, :tag =>"v#{version}"
 
   build_depends 'cmake', 'libboost-all-dev', 'leveldb'
   depends 'leveldb', 'libboost-filesystem1.46.1', 'libboost-program-options1.46.1',
