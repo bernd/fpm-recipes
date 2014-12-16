bb28c2ba86d8aa239b22440979af5a4cbd80b678
Bernd Ahlers <bernd@tuneafish.de>
2014-12-16 16:38:37 +0100

    Install bash completion.

--- password-store/recipe.rb
+++ password-store/recipe.rb
@@ -3,7 +3,7 @@ class PasswordStore < FPM::Cookery::Recipe
 
   name     'password-store'
   version  '1.6.3'
-  revision '2'
+  revision '3'
   homepage 'http://www.passwordstore.org/'
   license  'GPLv2+'
   source   'http://git.zx2c4.com/password-store', :with => :git, :tag => version
@@ -17,5 +17,7 @@ class PasswordStore < FPM::Cookery::Recipe
 
   def install
     make :install, 'DESTDIR' => destdir
+
+    etc('bash_completion.d').install 'src/completion/pass.bash-completion'
   end
 end
