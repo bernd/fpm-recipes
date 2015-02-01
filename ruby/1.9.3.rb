207cc855ada48324fd44abba8e1f68f1916a3aff
Doka <doka@wepoca.net>
2015-02-01 15:16:38 +0100

    update to v1.9.3 p551
    Update to Ruby v1.9.3 p551

--- ruby/1.9.3.rb
+++ ruby/1.9.3.rb
@@ -1,20 +1,20 @@
 class Ruby193 < FPM::Cookery::Recipe
-  description 'The Ruby virtual machine'
+  description 'The Ruby virtual machine v1.9.3 with p551''
 
   name 'ruby'
-  version '1:1.9.3.392'
+  version '1:1.9.3.551'
   revision 0
   homepage 'http://www.ruby-lang.org/'
-  source 'http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p392.tar.bz2'
-  sha256 '5a7334dfdf62966879bf539b8a9f0b889df6f3b3824fb52a9303c3c3d3a58391'
+  source 'http://cache.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p551.tar.gz'
+  sha256 'bb5be55cd1f49c95bb05b6f587701376b53d310eb1bb7c76fbd445a1c75b51e8'
 
   section 'interpreters'
 
-  build_depends 'autoconf', 'libreadline6-dev', 'bison', 'zlib1g-dev',
-                'libssl-dev', 'libyaml-dev', 'libncurses5-dev'
+  build_depends 'libffi-dev', 'libgdbm-dev', 'libncurses5-dev',
+                'libreadline6-dev', 'libssl-dev', 'libyaml-dev'
 
-  depends 'libffi6', 'libncurses5', 'libreadline6', 'libssl1.0.0', 'libtinfo5',
-          'libyaml-0-2', 'zlib1g'
+  depends 'libffi5', 'libncurses5', 'libreadline6', 'libssl1.0.0',
+          'libyaml-0-2', 'libgdbm3'
 
   def build
     configure :prefix => prefix, 'disable-install-doc' => true
