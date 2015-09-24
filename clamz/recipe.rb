29cf9ecb17355624f26bb8c492767420d79021b1
Bernd Ahlers <bernd@tuneafish.de>
2015-09-24 11:57:51 +0200

    Set epoch for clamz

--- clamz/recipe.rb
+++ clamz/recipe.rb
@@ -2,7 +2,7 @@ class Clamz < FPM::Cookery::Recipe
   description 'Downloader for the Amazon.com MP3 music store'
 
   name     'clamz'
-  version  '0.5'
+  version  '2:0.5'
   homepage 'http://code.google.com/p/clamz/'
   source   'http://clamz.googlecode.com/files/clamz-0.5.tar.gz'
   sha256   '5a63f23f15dfa6c2af00ff9531ae9bfcca0facfe5b1aa82790964f050a09832b'
