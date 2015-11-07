4b57c1fa16eb5074a92789c3de3ef39a354b9057
Bernd Ahlers <bernd@tuneafish.de>
2015-11-07 18:57:56 +0100

    Update to beanstalkd 1.10

--- beanstalkd/recipe.rb
+++ beanstalkd/recipe.rb
@@ -2,9 +2,9 @@ class Beanstalkd < FPM::Cookery::Recipe
   description 'A simple, fast work queue'
 
   name     'beanstalkd'
-  version  '1:1.5.0'
+  version  '1.10'
   homepage 'http://kr.github.com/beanstalkd/'
-  source   'http://github.com/kr/beanstalkd', :with => :git, :tag => 'v1.5'
+  source   'http://github.com/kr/beanstalkd', :with => :git, :tag => "v#{version}"
 
   section      'database'
   conflicts    'beanstalkd'
