ad05afdbfa2dece522f4c9e51f98d7b012143bb5
Bernd Ahlers <bernd@tuneafish.de>
2015-11-07 17:57:44 +0100

    Update tmux to 2.1

--- tmux/recipe.rb
+++ tmux/recipe.rb
@@ -2,13 +2,16 @@ class Tmux < FPM::Cookery::Recipe
   description 'terminal multiplexer'
 
   name     'tmux'
-  version  '1.8'
-  homepage 'http://tmux.sourceforge.net/'
-  source   "http://downloads.sourceforge.net/project/tmux/tmux/tmux-#{version}/tmux-#{version}.tar.gz"
-  sha1     '08677ea914e1973ce605b0008919717184cbd033'
+  version  '2.1'
+  homepage 'https://tmux.github.io/'
+  source   "https://github.com/tmux/tmux/releases/download/#{version}/tmux-#{version}.tar.gz"
+  sha1     '7b9a63f1d029498705e6a7bb980bc2f554d9d258'
 
   conflicts 'tmux'
   replaces  'tmux'
+
+  build_depends 'libncurses5-dev', 'libevent1-dev'
+  depends 'libevent-1.4-2'
 
   def build
     configure :prefix => prefix
