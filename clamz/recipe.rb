8171c9c3565ee5ee5e9dffdaf621ddc3d3549887
Bernd Ahlers <bernd@tuneafish.de>
2015-11-07 18:12:36 +0100

    Add dependencies for EL to clamz

--- clamz/recipe.rb
+++ clamz/recipe.rb
@@ -7,7 +7,13 @@ class Clamz < FPM::Cookery::Recipe
   source   'http://clamz.googlecode.com/files/clamz-0.5.tar.gz'
   sha256   '5a63f23f15dfa6c2af00ff9531ae9bfcca0facfe5b1aa82790964f050a09832b'
 
-  build_depends 'libgcrypt11-dev', 'libcurl4-gnutls-dev', 'libexpat1-dev'
+  platforms [:debian, :ubuntu] do
+    build_depends 'libgcrypt11-dev', 'libcurl4-gnutls-dev', 'libexpat1-dev'
+  end
+
+  platforms :centos do
+    build_depends 'expat-devel', 'curl-devel', 'libgcrypt-devel'
+  end
 
   def build
     configure :prefix => prefix
