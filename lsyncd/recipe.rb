b2dddde5716970fb49347845be61a70897f8fe02
Bernd Ahlers <bernd@tuneafish.de>
2015-11-09 21:49:09 +0100

    Update to lsyncd 2.1.6

--- lsyncd/recipe.rb
+++ lsyncd/recipe.rb
@@ -2,16 +2,18 @@ class Lsyncd < FPM::Cookery::Recipe
   description 'Live Syncing (Mirror) Daemon'
 
   name     'lsyncd'
-  version  '2.0.5'
-  homepage 'http://code.google.com/p/lsyncd/'
-  source   "http://lsyncd.googlecode.com/files/lsyncd-#{version}.tar.gz"
-  sha256   '3d76f26623a97de700764090abcc0d1e7ded50f386402567222dfd01a612121e'
+  version  '2.1.6'
+  revision '1'
 
-  build_depends 'liblua5.1-0-dev'
+  homepage 'https://github.com/axkibe/lsyncd'
+  source   "https://github.com/axkibe/lsyncd/archive/release-#{version}.tar.gz"
+  sha256   '02c241ee71b6abb23a796ac994a414e1229f530c249b838ae72d2ef74ae0f775'
+
+  build_depends 'liblua5.1-0-dev', 'cmake'
   depends       'liblua5.1-0', 'rsync', 'lua5.1'
 
   def build
-    configure :prefix => prefix
+    sh "cmake -D CMAKE_INSTALL_PREFIX=#{prefix} ."
     make
   end
 
