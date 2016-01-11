9c810dcd17f69af2d59c85644a8c143430009e99
Bernd Ahlers <bernd@tuneafish.de>
2016-01-11 10:18:03 +0100

    Update to vmtouch 1.0.2

--- vmtouch/recipe.rb
+++ vmtouch/recipe.rb
@@ -2,18 +2,19 @@ class VMTouch < FPM::Cookery::Recipe
   description 'the virtual memory toucher'
 
   name     'vmtouch'
-  version  '2012.06.12'
-  revision 0
-  source   'https://github.com/hoytech/vmtouch.git', :with => :git, :sha => '004b567d'
+  version  '1.0.2'
+  revision '1'
+
   homepage 'http://hoytech.com/vmtouch/'
+  source   "https://github.com/hoytech/vmtouch/archive/vmtouch-#{version}.tar.gz"
+  sha256   '48d41e5b805a0fcac46c12feeb4650e4b9def44b75d528d7c66c2f8b3a747c39'
 
   def build
     make
   end
 
   def install
-    make :install, 'DESTDIR' => destdir
-
+    bin.install 'vmtouch'
     man8.install 'vmtouch.8'
   end
 end
