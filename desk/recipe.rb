02e7049528d218c97921652398b3bceb130bab92
Bernd Ahlers <bernd@tuneafish.de>
2016-04-29 17:48:20 +0200

    Update desk to 0.5.0

--- desk/recipe.rb
+++ desk/recipe.rb
@@ -2,10 +2,10 @@ class Desk < FPM::Cookery::Recipe
   description 'Lightweight workspace manager for the shell'
 
   name 'desk'
-  version '0.4.0'
+  version '0.5.0'
 
   source "https://github.com/jamesob/desk/archive/v#{version}.tar.gz"
-  sha256 '1589cb02d1104b4008d9d1a1ee1a315d0bf5a8e8d6f8408d9f60201a4f04c896'
+  sha256 '4ed7a2547fdb3e442e9456be7ca27c41f3fc580d3fdffd69fcfed84260ca2576'
 
   def build
   end
