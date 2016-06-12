b35675443d69435f277195d7f50ade15317058f8
Bernd Ahlers <bernd@tuneafish.de>
2016-06-12 13:50:51 +0200

    Better description for snzip

--- snzip/recipe.rb
+++ snzip/recipe.rb
@@ -1,9 +1,11 @@
 class Snzip < FPM::Cookery::Recipe
-  description 'snappy cli'
+  description 'a compression/decompression tool based on snappy'
 
   name 'snzip'
   version '1.0.3'
+  revision '1'
 
+  homepage 'https://github.com/kubo/snzip'
   source "https://bintray.com/artifact/download/kubo/generic/snzip-#{version}.tar.gz"
   sha256 'c83f1301cb1f1b64a25ef10e5fcfc2f6f66fa092ae833c524cad219c0ef2e990'
 
