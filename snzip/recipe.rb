2c6d38ffcb1f790cb61bb4cc664a94e22f9bc802
Bernd Ahlers <bernd@tuneafish.de>
2016-06-14 19:06:16 +0200

    Missing snappy deps for snzip

--- snzip/recipe.rb
+++ snzip/recipe.rb
@@ -9,6 +9,9 @@ class Snzip < FPM::Cookery::Recipe
   source "https://bintray.com/artifact/download/kubo/generic/snzip-#{version}.tar.gz"
   sha256 'c83f1301cb1f1b64a25ef10e5fcfc2f6f66fa092ae833c524cad219c0ef2e990'
 
+  build_depends 'libsnappy-dev'
+  depends 'libsnappy1v5'
+
   def build
     configure :prefix => prefix
     make
