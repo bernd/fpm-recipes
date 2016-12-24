8da2ed117b638b96398642917d0db6be0549b47e
Bernd Ahlers <bernd@tuneafish.de>
2016-12-24 10:54:04 +0100

    Update to devd-0.7

--- devd/recipe.rb
+++ devd/recipe.rb
@@ -2,10 +2,10 @@ class Devd < FPM::Cookery::Recipe
   description 'A local webserver for developers'
 
   name 'devd'
-  version '0.6'
+  version '0.7'
 
   source "https://github.com/cortesi/devd/releases/download/v#{version}/devd-#{version}-linux64.tgz"
-  sha256 '93bf7590cc5aa42cf8521795e32992ba479ba2aed825fb6dac2b27d02e293575'
+  sha256 '3bf919c081f46d4a0e1e5c8f35d943b949db39811f4b99177d90465ebb17fd4c'
 
   def build
   end
