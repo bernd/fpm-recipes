b984955e94c4e6d326f9cc64e3ebb108d9e94fca
Bernd Ahlers <bernd@tuneafish.de>
2016-12-24 11:02:50 +0100

    Update to snzip-1.0.4

--- snzip/recipe.rb
+++ snzip/recipe.rb
@@ -2,12 +2,12 @@ class Snzip < FPM::Cookery::Recipe
   description 'a compression/decompression tool based on snappy'
 
   name 'snzip'
-  version '1.0.3'
+  version '1.0.4'
   revision '1'
 
   homepage 'https://github.com/kubo/snzip'
   source "https://bintray.com/artifact/download/kubo/generic/snzip-#{version}.tar.gz"
-  sha256 'c83f1301cb1f1b64a25ef10e5fcfc2f6f66fa092ae833c524cad219c0ef2e990'
+  sha256 'a45081354715d48ed31899508ebed04a41d4b4a91dca37b79fc3b8ee0c02e25e'
 
   build_depends 'libsnappy-dev'
   depends 'libsnappy1v5'
