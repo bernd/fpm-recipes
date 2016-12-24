b02c3fc9e58d5b8fb53291597d9f8d1456c316ab
Bernd Ahlers <bernd@tuneafish.de>
2016-12-24 11:00:38 +0100

    Update to noti-2.5.0

--- noti/recipe.rb
+++ noti/recipe.rb
@@ -2,11 +2,11 @@ class Noti < FPM::Cookery::Recipe
   description 'Trigger notifications when a process completes'
 
   name     'noti'
-  version  '2.2.2'
+  version  '2.5.0'
   revision '1'
 
   source "https://github.com/variadico/noti/releases/download/v#{version}/noti#{version}.linux-amd64.tar.gz"
-  sha256 '544178e74c0dc42434527db1d530f07440f4d2ff6a0e36584d311bf17629508e'
+  sha256 'ba8941a0d0838425b82ffc89698d92e94b13ddbe13f1270da57eb76ae4b262ee'
 
   def build
   end
