14cf067f2915ac424a1a68abaa08835b6f94bdc1
Bernd Ahlers <bernd@tuneafish.de>
2017-01-31 09:20:43 +0100

    Update to ripgrep 0.4.0

--- ripgrep/recipe.rb
+++ ripgrep/recipe.rb
@@ -2,12 +2,12 @@ class Ripgrep < FPM::Cookery::Recipe
   description 'combines the usability of The Silver Searcher with the raw speed of grep'
 
   name     'ripgrep'
-  version  '0.3.2'
+  version  '0.4.0'
   revision '1'
 
   homepage 'https://github.com/BurntSushi/ripgrep/releases'
   source   "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-unknown-linux-musl.tar.gz"
-  sha256   '0f81f318e0fae816565d3490b99d27b780fb54ff3836d4fbbaa524a7321a00ca'
+  sha256   'fe99ba104f3b28ed15d0c83a30ef0b2e6286a27710f88fff77c108873f181c3b'
 
   def build
   end
