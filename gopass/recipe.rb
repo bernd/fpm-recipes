9906e3b4177c6929eef60e5b74ab7a2c9cfcdfff
Bernd Ahlers <bernd@tuneafish.de>
2017-10-10 09:43:03 +0200

    Update to gopass 1.4.1

--- gopass/recipe.rb
+++ gopass/recipe.rb
@@ -2,12 +2,12 @@ class Gopass < FPM::Cookery::Recipe
   description 'slightly more awesome standard unix password manager for teams'
 
   name     'gopass'
-  version  '1.4.0-rc3'
+  version  '1.4.1'
   revision '1'
 
   homepage 'https://github.com/justwatchcom/gopass/releases'
   source   "https://github.com/justwatchcom/gopass/releases/download/v#{version}/gopass-#{version}-linux-amd64.tar.gz"
-  sha256   'ed233b3d2773dc7b1839f6b7a0152692019e9370bbfb9b95ed50b7590f187097'
+  sha256   '6c46e9f76d760be2d709b7d3561260dd6e908cff406cf590a62ac5e6a00e16a9'
 
   def build
   end
