76c8576cb144a049a828190863161f769a5cf889
Bernd Ahlers <bernd@tuneafish.de>
2018-01-10 20:52:48 +0100

    Switch back to upstream version

--- godeb/recipe.rb
+++ godeb/recipe.rb
@@ -1,18 +1,14 @@
 class Godeb < FPM::Cookery::Recipe
   description 'In-flight .deb packages for Go'
 
-  #homepage 'https://github.com/niemeyer/godeb'
-  # Fixed version:
-  homepage 'https://github.com/mgit-at/godeb'
+  homepage 'https://github.com/niemeyer/godeb'
 
   name     'godeb'
-  version  '20171116'
+  version  '20180109'
   revision '1'
 
-  #source 'https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz'
-  # From the fixed version
-  source 'https://storage.googleapis.com/godeb/godeb-amd64.tar.gz'
-  sha256 'f976fd86698db232379c5d0df6cd01ec0fc95e7826325a9d6ed1c4d96aae2bb3'
+  source 'https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz'
+  sha256 '4e73d1621495cc2b909893b9d31d74caec9110c0000218d123d8515f87e9c3ff'
 
   def build
   end
