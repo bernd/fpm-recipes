734a09a7706a3ba72f65557939fe5656dcdfac82
Bernd Ahlers <bernd@tuneafish.de>
2018-01-13 21:06:16 +0100

    Update to hugo 0.32.4

--- hugo/recipe.rb
+++ hugo/recipe.rb
@@ -2,17 +2,17 @@ class Hugo < FPM::Cookery::Recipe
   description 'Fast and Flexible Static Site Generator built with love in GoLang'
 
   name 'hugo'
-  version '0.20.1'
+  version '0.32.4'
   revision '1'
 
   homepage 'http://gohugo.io/'
   source "https://github.com/spf13/hugo/releases/download/v#{version}/hugo_#{version}_Linux-64bit.tar.gz"
-  sha256 '02cc04e30da6ba2dbf7ddee1a1778e8a0e6adf3cbab074a224ba8d5d472049d1'
+  sha256 '004e437e4afa7a3556019b4215767d6823435d9a4c5b6f82715b93ac93bea44f'
 
   def build
   end
 
   def install
-    bin.install "hugo_#{version}_linux_amd64", 'hugo'
+    bin.install 'hugo'
   end
 end
