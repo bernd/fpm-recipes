80601b4416d889d5bbbea18b5703a3b18145cff7
Bernd Ahlers <bernd@tuneafish.de>
2018-02-12 12:39:39 +0100

    Update to ripgrep 0.8.0

--- ripgrep/recipe.rb
+++ ripgrep/recipe.rb
@@ -1,19 +1,20 @@
 class Ripgrep < FPM::Cookery::Recipe
-  description 'combines the usability of The Silver Searcher with the raw speed of grep'
+  description 'recursively searches directories for a regex pattern'
 
   name     'ripgrep'
-  version  '0.5.1'
+  version  '0.8.0'
   revision '1'
 
-  homepage 'https://github.com/BurntSushi/ripgrep/releases'
+  homepage 'https://github.com/BurntSushi/ripgrep'
   source   "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-unknown-linux-musl.tar.gz"
-  sha256   '4c20367910a7556524d8c946a7de1b82d103baaafc83edc97578a18063375d78'
+  sha256   '621f2f16f65203aa37e7c10ecfb22384c4c01e70ebbd30a13c0d6944ffc6e59e'
 
   def build
   end
 
   def install
     bin.install 'rg'
-    man1.install 'rg.1'
+    man1.install 'doc/rg.1'
+    doc('ripgrep').install Dir['doc/*.md']
   end
 end
