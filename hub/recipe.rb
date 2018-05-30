c6d9a3fc621309da0d8ec444b3ac69d93f23178c
Bernd Ahlers <bernd@tuneafish.de>
2018-05-30 14:08:32 +0200

    Update to hub 2.3.0

--- hub/recipe.rb
+++ hub/recipe.rb
@@ -2,19 +2,21 @@ class Hub < FPM::Cookery::Recipe
   description 'hub helps you win at git'
 
   name     'hub'
-  version  '2.2.9'
+  version  '2.3.0'
   revision '1'
   arch     FPM::Cookery::Facts.arch
   homepage 'http://hub.github.com/'
   source   "https://github.com/github/hub/releases/download/v#{version}/hub-linux-#{arch}-#{version}.tgz"
-  sha256   '5867647071007add582f911867fb4973354c51690ac20aecb2e73be7440875a3'
+  sha256   '45b50fc52cf203dc16e4866217c8d0ca70b5f78668b4218e6697b118bcfce490'
 
   def build
   end
 
   def install
     bin.install 'bin/hub'
-    man1.install 'share/man/man1/hub.1'
+    Dir['share/man/man1/*.1'].each do |manfile|
+      man1.install manfile
+    end
     etc('bash_completion.d').install 'etc/hub.bash_completion.sh', 'hub'
   end
 end
