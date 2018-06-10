d9cf4acdc13df8c5ed2da73c6fa147af1e6cf765
Bernd Ahlers <bernd@tuneafish.de>
2018-06-10 12:13:43 +0200

    Fix source location for clamz

--- clamz/recipe.rb
+++ clamz/recipe.rb
@@ -4,7 +4,7 @@ class Clamz < FPM::Cookery::Recipe
   name     'clamz'
   version  '2:0.5'
   homepage 'http://code.google.com/p/clamz/'
-  source   'http://clamz.googlecode.com/files/clamz-0.5.tar.gz'
+  source   'https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/clamz/clamz-0.5.tar.gz'
   sha256   '5a63f23f15dfa6c2af00ff9531ae9bfcca0facfe5b1aa82790964f050a09832b'
 
   platforms [:debian, :ubuntu] do
