d0cdf21fef83f5f0c6e623a8cd3c5860b63aaca0
Bernd Ahlers <bernd@tuneafish.de>
2018-06-10 12:14:51 +0200

    Update to couchdb 2.1.1

--- couchdb/recipe.rb
+++ couchdb/recipe.rb
@@ -2,21 +2,21 @@ class CouchDB < FPM::Cookery::Recipe
   description 'RESTful document oriented database'
 
   name     'couchdb'
-  version  '2.0.0'
+  version  '2.1.1'
   revision '1'
 
   source "http://apache.mirror.iphh.net/couchdb/source/#{version}/apache-couchdb-#{version}.tar.gz"
-  sha256 'ccaf3ce9cb06c50a73e091696e557e2a57c5ba02c5b299e1ac2f5b959ee96eca'
+  sha256 'd5f255abc871ac44f30517e68c7b30d1503ec0f6453267d641e00452c04e7bcc'
 
   build_depends 'build-essential', 'pkg-config', 'erlang', 'libicu-dev',
-	        'libmozjs185-dev', 'libcurl4-openssl-dev', 'help2man',
-		'python-sphinx'
+                'libmozjs185-dev', 'libcurl4-openssl-dev', 'help2man',
+                'python-sphinx'
 
   pre_install    'pre-install'
   post_install   'post-install'
   pre_uninstall  'pre-uninstall'
   post_uninstall 'post-uninstall'
-  
+
   # Execute the following to make sure the build works:
   #     sudo touch /usr/lib/erlang/man/man1/gcov-tool-5.1.gz
   # See: https://bugs.launchpad.net/ubuntu/+source/erlang/+bug/1600780
