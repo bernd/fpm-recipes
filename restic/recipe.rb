438258e6bde6a4c41e04bdc1bc9fdcae05e29150
Bernd Ahlers <bernd@tuneafish.de>
2018-08-10 09:02:02 +0200

    Update to restic 0.9.2

--- restic/recipe.rb
+++ restic/recipe.rb
@@ -4,11 +4,11 @@ class Restic < FPM::Cookery::Recipe
   homepage 'https://restic.github.io/'
 
   name     'restic'
-  version  '0.9.1'
+  version  '0.9.2'
   revision '1'
 
   source "https://github.com/restic/restic/releases/download/v#{version}/restic_#{version}_linux_amd64.bz2"
-  sha256 'f7f76812fa26ca390029216d1378e5504f18ba5dde790878dfaa84afef29bda7'
+  sha256 '1ad8a76a9f966da5f7a319c49a6db071a60ebaa24d69e6d86d53d6f2bcaf11ed'
 
   def extract
     extracted_source = "#{builddir}/restic-#{version}"
