8659700c3b67de53140d0a38a22c66d58e463660
Bernd Ahlers <bernd@tuneafish.de>
2018-11-23 13:05:08 +0100

    Update to docker-compose 1.23.1

--- docker-compose/recipe.rb
+++ docker-compose/recipe.rb
@@ -4,11 +4,11 @@ class DockerCompose < FPM::Cookery::Recipe
   homepage 'https://docs.docker.com/compose/'
 
   name 'docker-compose'
-  version '1.22.0'
+  version '1.23.1'
   revision '1'
 
   source "https://github.com/docker/compose/releases/download/#{version}/docker-compose-Linux-x86_64"
-  sha256 'f679a24b93f291c3bffaff340467494f388c0c251649d640e661d509db9d57e9'
+  sha256 'c176543737b8aea762022245f0f4d58781d3cb1b072bc14f3f8e5bb96f90f1a2'
 
   def build
   end
