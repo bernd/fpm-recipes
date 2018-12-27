f28b250f1163852ee39fc8168a1b5e863499f868
Bernd Ahlers <bernd@tuneafish.de>
2018-12-27 12:39:07 +0100

    Update to ripgrep 0.10.0

--- ripgrep/recipe.rb
+++ ripgrep/recipe.rb
@@ -2,12 +2,12 @@ class Ripgrep < FPM::Cookery::Recipe
   description 'recursively searches directories for a regex pattern'
 
   name     'ripgrep'
-  version  '0.9.0'
+  version  '0.10.0'
   revision '1'
 
   homepage 'https://github.com/BurntSushi/ripgrep'
   source   "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-unknown-linux-musl.tar.gz"
-  sha256   '2eb4443e58f95051ff76ea036ed1faf940d5a04af4e7ff5a7dbd74576b907e99'
+  sha256   'c76080aa807a339b44139885d77d15ad60ab8cdd2c2fdaf345d0985625bc0f97'
 
   def build
   end
