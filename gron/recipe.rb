12c455d31fbf6f3fa0a31573bf88460766d61c6a
Bernd Ahlers <bernd@tuneafish.de>
2018-12-27 12:35:18 +0100

    Update to gron 0.6.0

--- gron/recipe.rb
+++ gron/recipe.rb
@@ -4,11 +4,11 @@ class Gron < FPM::Cookery::Recipe
   homepage 'https://github.com/tomnomnom/gron'
 
   name     'gron'
-  version  '0.5.1'
+  version  '0.6.0'
   revision '1'
 
   source "https://github.com/tomnomnom/gron/releases/download/v#{version}/gron-linux-amd64-#{version}.tgz"
-  sha256 'f874724398de74f14f047366c3e2cb1be690dbd2000b22b1b1f515d78fce3982'
+  sha256 '891470b44d7faef49683dd11b11a1f296324118e869a76e3fae61cf6b35c6b74'
 
   def build
   end
