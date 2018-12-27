d59fe285663c74490139415df3a75827064115a5
Bernd Ahlers <bernd@tuneafish.de>
2018-12-27 13:25:14 +0100

    Update to htop 2.2.0

--- htop/recipe.rb
+++ htop/recipe.rb
@@ -2,12 +2,12 @@ class Htop < FPM::Cookery::Recipe
   description 'interactive process viewer'
 
   name    'htop'
-  version '2.0.0'
+  version '2.2.0'
 
   homepage 'http://hisham.hm/htop/'
 
   source   "http://hisham.hm/htop/releases/#{version}/htop-#{version}.tar.gz"
-  sha256   'd15ca2a0abd6d91d6d17fd685043929cfe7aa91199a9f4b3ebbb370a2c2424b5'
+  sha256   'd9d6826f10ce3887950d709b53ee1d8c1849a70fa38e91d5896ad8cbc6ba3c57'
 
   build_depends 'libncursesw5-dev'
   depends 'libncursesw5'
