e5ed4d41da86f11f318b0279b23c1b69019d6ff1
Bernd Ahlers <bernd@tuneafish.de>
2018-12-27 13:12:06 +0100

    Update to liteide 35.3

--- liteide/recipe.rb
+++ liteide/recipe.rb
@@ -2,12 +2,12 @@ class LiteIDE < FPM::Cookery::Recipe
   description 'A simple, open source, cross-platform Go IDE'
 
   name     'liteide'
-  version  '32'
+  version  '35.3'
   revision '1'
 
-  source   "https://downloads.sourceforge.net/project/liteide/X#{version}/liteidex#{version}-2.linux64-qt4.tar.bz2"
+  source   "https://github.com/visualfc/liteide/releases/download/x#{version}/liteidex#{version}.linux64-qt5.5.1.tar.gz"
   homepage 'https://github.com/visualfc/liteide'
-  sha256   '446904005452a420b5c4ee8b2f8ae97ce2fcefdde98521c969d602453e211702'
+  sha256   'e19a918b805eac8ec0c3fd3177b5e1bff11c7793d3ccb04883fc352f9c3b0b42'
 
   def build
     File.open('liteide.sh', 'w', 0755) do |file|
