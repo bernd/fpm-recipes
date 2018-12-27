1b4e0609c81042773314310bd1836e03a0acab67
Bernd Ahlers <bernd@tuneafish.de>
2018-12-27 12:50:23 +0100

    Update to fd 7.2.0

--- fd/recipe.rb
+++ fd/recipe.rb
@@ -2,18 +2,18 @@ class Fd < FPM::Cookery::Recipe
   description 'A simple, fast and user-friendly alternative to find'
 
   name     'fd'
-  version  '4.0.0'
+  version  '7.2.0'
   revision '1'
 
   homepage 'https://github.com/sharkdp/fd'
-  source   "https://github.com/sharkdp/fd/releases/download/v#{version}/fd-v#{version}-x86_64-linux.tar.gz"
-  sha256   '7481bd9650005d5095be907767f5d6278737348448cb07b7d625d314113cf737'
+  source   "https://github.com/sharkdp/fd/releases/download/v#{version}/fd-v#{version}-x86_64-unknown-linux-musl.tar.gz"
+  sha256   '63ba7122ad37a3df48a0faff0b33d249694c7e8c2c8cd1f3af5a5fc5b7b74a09'
 
   def build
   end
 
   def install
     bin.install 'fd'
-    etc('bash_completion.d').install 'fd.bash-completion', 'fd'
+    etc('bash_completion.d').install 'autocomplete/fd.bash-completion', 'fd'
   end
 end
