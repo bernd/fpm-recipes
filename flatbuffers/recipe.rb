b39a289845d26e1754cee71559fdf5a48c646be5
Bernd Ahlers <bernd@tuneafish.de>
2018-12-27 13:18:02 +0100

    Update to flatbuffers 1.10.0

--- flatbuffers/recipe.rb
+++ flatbuffers/recipe.rb
@@ -2,12 +2,12 @@ class Flatbuffers < FPM::Cookery::Recipe
   description 'efficient cross platform serialization library'
 
   name     'flatbuffers'
-  version  '1.9.0'
+  version  '1.10.0'
   revision '1'
 
   source   "https://github.com/google/flatbuffers/archive/v#{version}.tar.gz"
   homepage 'https://google.github.io/flatbuffers/'
-  sha256   '5ca5491e4260cacae30f1a5786d109230db3f3a6e5a0eb45d0d0608293d247e3'
+  sha256   '3714e3db8c51e43028e10ad7adffb9a36fc4aa5b1a363c2d0c4303dd1be59a7c'
 
   build_depends 'cmake'
 
