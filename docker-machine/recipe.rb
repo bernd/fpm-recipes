421af29ba218db698a5ba6c8e89ad6526ad84915
Bernd Ahlers <bernd@tuneafish.de>
2019-07-27 00:31:23 +0200

    Update to docker-machine 0.16.1

--- docker-machine/recipe.rb
+++ docker-machine/recipe.rb
@@ -4,11 +4,11 @@ class DockerMachine < FPM::Cookery::Recipe
   homepage 'https://docs.docker.com/machine/'
 
   name 'docker-machine'
-  version '0.16.0'
+  version '0.16.1'
   revision '1'
 
   source "https://github.com/docker/machine/releases/download/v#{version}/docker-machine-Linux-x86_64"
-  sha256 '3d2cae5f66df636116153c8a96310c0cdef0060b1ecfe86116cec825bbf393d8'
+  sha256 '44a008c14549156222b314b1448c22ef255b446419fcf96570f3f288dff318a9'
 
   def build
   end
