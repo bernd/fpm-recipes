bdb0b5343d6cd380168d1aeec48aa55a6024b067
Bernd Ahlers <bernd@tuneafish.de>
2019-08-10 13:51:43 +0200

    Update to jid 0.7.6

--- jid/recipe.rb
+++ jid/recipe.rb
@@ -2,17 +2,17 @@ class Jid < FPM::Cookery::Recipe
   description 'JSON incremental digger'
 
   name     'jid'
-  version  '0.7.2'
+  version  '0.7.6'
   revision '1'
 
   homepage 'https://github.com/simeji/jid'
-  source   "https://github.com/simeji/jid/releases/download/#{version}/jid_linux_amd64.zip"
-  sha256   '57d9e6cb6dcd052d7ffcfddd7bc88fd5a07351a9bdf0575d3355a9921153e177'
+  source   "https://github.com/simeji/jid/releases/download/v#{version}/jid_linux_amd64.zip"
+  sha256   'b5706cda44bc76c5db8a1e7956d25ef2e25bf618a7b03e1d98e8f4fc2e2f393a'
 
   def build
   end
 
   def install
-    bin.install 'jid_linux_amd64', 'jid'
+    bin.install 'jid'
   end
 end
