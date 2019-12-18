32430706a93494a31570e70867fa980c76163fe6
Bernd Ahlers <bernd@tuneafish.de>
2019-12-18 18:48:03 +0100

    Update to curlie 1.3.1

--- curlie/recipe.rb
+++ curlie/recipe.rb
@@ -2,12 +2,12 @@ class Curlie < FPM::Cookery::Recipe
   description 'The power of curl, the ease of use of httpie.'
 
   name 'curlie'
-  version '1.2.0'
+  version '1.3.1'
   revision '1'
 
   homepage 'https://curlie.io/'
   source "https://github.com/rs/curlie/releases/download/v#{version}/curlie_#{version}_linux_amd64.tar.gz"
-  sha256 'ff27eaaa5b3540e0924a4096c55ec70001f46564abaa005ac92c48a94a62fe32'
+  sha256 '5536b0643c908066bf40e87708d4c6a534dcf8385df7dc15fa34ac06538846a1'
 
   def build
   end
