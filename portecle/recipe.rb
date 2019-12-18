a0b48856fd4e22110ff8da76c0d52d176b0c6115
Bernd Ahlers <bernd@tuneafish.de>
2019-12-18 18:38:45 +0100

    Update to portecle 1.11

--- portecle/recipe.rb
+++ portecle/recipe.rb
@@ -2,25 +2,26 @@ class Portecle < FPM::Cookery::Recipe
   description 'user friendly GUI application for creating, managing and examining keystores'
 
   name 'portecle'
-  version '1.10'
-  revision '2'
+  version '1.11'
+  revision '0'
 
-  homepage 'http://portecle.sourceforge.net/'
-  source "http://downloads.sourceforge.net/project/portecle/portecle/1.10/portecle-1.10.zip"
-  sha256 '352b97d114e52d4ef5452e887a058a376e347ab36159c2e594efee4b1b5b2e01'
+  homepage 'https://github.com/scop/portecle'
+  source "https://github.com/scop/portecle/releases/download/v#{version}/portecle-#{version}.zip"
+  sha256 '75da1d5552979f3310adf4c93f90da00b56dfa12424ab52047c0c937ab6709bb'
 
   arch 'all'
 
   def build
-    Dir.chdir("portecle-#{version}") do
-      patch(workdir('portecle.desktop.patch'))
-    end
   end
 
   def install
     Dir.chdir("portecle-#{version}") do
-      share('icons/hicolor/32x32/apps').install 'portecle.png'
-      share('applications').install 'portecle.desktop'
+      %w(16x16 32x32 48x48 64x64 96x96 128x128 192x192 256x256 512x512).each do |size|
+        share("icons/hicolor/#{size}/apps").install Dir["icons/#{size}/*"]
+      end
+      rm_rf 'icons'
+      share('applications').install 'net.sf.portecle.desktop'
+      share('appdata').install 'net.sf.portecle.appdata.xml'
       share('portecle').install Dir['*']
       bin.install workdir('portecle.sh'), 'portecle'
 
